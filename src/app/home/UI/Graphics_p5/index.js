da5e667e010120d99e6df01d34450fd14c2ebf60
Jonathan Richards <hello@jonnycolby.com>
2025-03-24 13:02:04 -0600

    Removing the 2D "tap hint", declaring noLoop() on 2D animation, redraw() on window resize, cleanup

--- src/app/home/UI/Graphics_p5/index.js
+++ src/app/home/UI/Graphics_p5/index.js
@@ -24,7 +24,7 @@ class Graphics extends React.Component {
         };
 
         Z.state = {
-            //
+            // ...
         };
 
         Z.vars = {
@@ -37,15 +37,11 @@ class Graphics extends React.Component {
                 p5: null,
                 stats: null,
             },
-            image: {
-                //
-            },
+            image: {},
             px: {
                 corner: null,
             },
-            graphics: {
-                background_noise: null,
-            },
+            graphics: {},
             tap_hint: null,
         };
 
@@ -176,13 +172,13 @@ class Graphics extends React.Component {
             p5.createCanvas(Z.cache.graphics.bbox.width, Z.cache.graphics.bbox.height);
             // p5.pixelDensity(1);
             // p5.frameRate(60);
-            // p5.noLoop();
+            p5.noLoop();
         };
 
         p5.windowResized = () => {
             Z.measure();
             p5.resizeCanvas(Z.cache.graphics.bbox.width, Z.cache.graphics.bbox.height);
-            // p5.redraw();
+            p5.redraw();
         };
 
         p5.draw = () => {
@@ -201,8 +197,6 @@ class Graphics extends React.Component {
                 y: Math.floor(cols_count * 0.75),
                 // y: Math.floor(rows_count * 0.5),
             };
-
-            // p5.image(Z.vars.background_noise, 0, 0, p5.width, p5.height);
 
             // MARK: top-left corner mark
             p5.push();
@@ -236,106 +230,6 @@ class Graphics extends React.Component {
                 }
             }
             p5.pop();
-
-            //
-            // MARK: "tap" hint
-            //
-            // if (typeof DeviceOrientationEvent.requestPermission === "function") {
-            p5.push();
-            p5.textSize(18);
-            p5.textAlign(p5.LEFT, p5.CENTER);
-            //
-            // TODO: move these to class instance vars to avoid redeclaration on every frame
-            Z.mem.tap_hint = {
-                pos: { x: p5.width * 0.75, y: p5.height * 0.25 },
-                radius: 13,
-                radius_variance: 5,
-                radius_current: null,
-                line_space: 16,
-                line_length: 64,
-                word_space: 16,
-                speed: 1.0,
-                text: "Tap",
-                hint_delay_sec: 2.0,
-                // hint_appear_speed_sec: 1.0,
-                //
-                total_width: null,
-                mobile_pos: null,
-                final_pos: null,
-            };
-            const tap_hint = Z.mem.tap_hint;
-            tap_hint.total_width = tap_hint.radius + tap_hint.line_length + tap_hint.word_space + p5.textWidth(tap_hint.text);
-            tap_hint.mobile_pos = {
-                x: p5.width - tap_hint.total_width - tap_hint.word_space * 2.0,
-                y: p5.height * 0.15,
-            };
-            if (tap_hint.mobile_pos.x < tap_hint.pos.x) tap_hint.final_pos = tap_hint.mobile_pos;
-            else tap_hint.final_pos = tap_hint.pos;
-
-            //
-            //  MARK: "tap" hint animation (TODO)
-            //
-            if (_now > 1000 * 5.0) {
-                var progress_sec = (_now - 1000 * 5.0) * 0.001;
-                progress_sec = progress_sec * 2.0;
-                // console.log("progress_sec:", progress_sec);
-                tap_hint.opacity = Math.min(1.0, progress_sec / 1.0);
-                // tap_hint.opacity = Math.pow(tap_hint.opacity, 5.0);
-                console.log("tap_hint.opacity:", tap_hint.opacity);
-                // tap_hint.opacity = Math.pow(Math.min(1.0, Math.max(0.0, (_now - 1000 * tap_hint.hint_delay_sec) * 0.00004)), 3.0);
-                // console.log("tap_hint.opacity:", tap_hint.opacity);
-                // tap_hint.opacity = Max.max(1.0, )
-                var tap_hint_color = p5.color(243, 242, 243);
-                tap_hint_color.setAlpha(tap_hint.opacity * 255);
-                //
-                p5.push();
-                p5.noFill();
-                p5.stroke(tap_hint_color);
-                p5.strokeWeight(2);
-                //
-                p5.push();
-                var tap_circ_progress = Math.sin(_now * 0.004 * tap_hint.speed) * 0.5 + 0.5;
-                tap_circ_progress = 1.0 - Math.pow(1.0 - tap_circ_progress, 3.0);
-                tap_hint.radius_current = tap_hint.radius + tap_hint.radius_variance * (tap_circ_progress - 0.5);
-                p5.circle(tap_hint.final_pos.x, tap_hint.final_pos.y, tap_hint.radius_current * 2.0);
-                p5.pop();
-                //
-                p5.push();
-                p5.line(
-                    tap_hint.final_pos.x + tap_hint.radius_current + tap_hint.line_space, //
-                    // tap_hint.final_pos.x + tap_hint.radius + tap_hint.line_space, //
-                    tap_hint.final_pos.y,
-                    tap_hint.final_pos.x + tap_hint.radius + tap_hint.line_length,
-                    tap_hint.final_pos.y,
-                );
-                p5.pop();
-                //
-                p5.push();
-                p5.fill(tap_hint_color);
-                p5.noStroke();
-                p5.text(
-                    tap_hint.text, //
-                    tap_hint.final_pos.x + tap_hint.radius + tap_hint.line_length + tap_hint.word_space,
-                    tap_hint.final_pos.y,
-                );
-                p5.pop();
-                //
-                // // for DEBUG:
-                // p5.push();
-                // p5.fill(243, 0, 1);
-                // p5.noStroke();
-                // p5.rect(
-                //     tap_hint.final_pos.x, //
-                //     tap_hint.final_pos.y,
-                //     tap_hint.total_width,
-                //     1,
-                // );
-                // p5.pop();
-                //
-                p5.pop();
-                // }
-            }
-            //
 
             //
             p5.pop();
