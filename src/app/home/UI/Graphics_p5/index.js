736ea7eccc52b019258c21e6cc75a9a91f222a75
Jonathan Richards <hello@jonnycolby.com>
2025-03-24 12:40:00 -0600

    Reviving the 2D "tap hint" from commented code

--- src/app/home/UI/Graphics_p5/index.js
+++ src/app/home/UI/Graphics_p5/index.js
@@ -176,11 +176,13 @@ class Graphics extends React.Component {
             p5.createCanvas(Z.cache.graphics.bbox.width, Z.cache.graphics.bbox.height);
             // p5.pixelDensity(1);
             // p5.frameRate(60);
+            // p5.noLoop();
         };
 
         p5.windowResized = () => {
             Z.measure();
             p5.resizeCanvas(Z.cache.graphics.bbox.width, Z.cache.graphics.bbox.height);
+            // p5.redraw();
         };
 
         p5.draw = () => {
@@ -270,70 +272,70 @@ class Graphics extends React.Component {
             if (tap_hint.mobile_pos.x < tap_hint.pos.x) tap_hint.final_pos = tap_hint.mobile_pos;
             else tap_hint.final_pos = tap_hint.pos;
 
-            // //
-            // //  MARK: "tap" hint animation (TODO)
-            // //
-            // if (_now > 1000 * 5.0) {
-            //     var progress_sec = (_now - 1000 * 5.0) * 0.001;
-            //     progress_sec = progress_sec * 2.0;
-            //     // console.log("progress_sec:", progress_sec);
-            //     tap_hint.opacity = Math.min(1.0, progress_sec / 1.0);
-            //     // tap_hint.opacity = Math.pow(tap_hint.opacity, 5.0);
-            //     console.log("tap_hint.opacity:", tap_hint.opacity);
-            //     // tap_hint.opacity = Math.pow(Math.min(1.0, Math.max(0.0, (_now - 1000 * tap_hint.hint_delay_sec) * 0.00004)), 3.0);
-            //     // console.log("tap_hint.opacity:", tap_hint.opacity);
-            //     // tap_hint.opacity = Max.max(1.0, )
-            //     var tap_hint_color = p5.color(243, 242, 243);
-            //     tap_hint_color.setAlpha(tap_hint.opacity * 255);
-            //     //
-            //     p5.push();
-            //     p5.noFill();
-            //     p5.stroke(tap_hint_color);
-            //     p5.strokeWeight(2);
-            //     //
-            //     p5.push();
-            //     var tap_circ_progress = Math.sin(_now * 0.004 * tap_hint.speed) * 0.5 + 0.5;
-            //     tap_circ_progress = 1.0 - Math.pow(1.0 - tap_circ_progress, 3.0);
-            //     tap_hint.radius_current = tap_hint.radius + tap_hint.radius_variance * (tap_circ_progress - 0.5);
-            //     p5.circle(tap_hint.final_pos.x, tap_hint.final_pos.y, tap_hint.radius_current * 2.0);
-            //     p5.pop();
-            //     //
-            //     p5.push();
-            //     p5.line(
-            //         tap_hint.final_pos.x + tap_hint.radius_current + tap_hint.line_space, //
-            //         // tap_hint.final_pos.x + tap_hint.radius + tap_hint.line_space, //
-            //         tap_hint.final_pos.y,
-            //         tap_hint.final_pos.x + tap_hint.radius + tap_hint.line_length,
-            //         tap_hint.final_pos.y,
-            //     );
-            //     p5.pop();
-            //     //
-            //     p5.push();
-            //     p5.fill(tap_hint_color);
-            //     p5.noStroke();
-            //     p5.text(
-            //         tap_hint.text, //
-            //         tap_hint.final_pos.x + tap_hint.radius + tap_hint.line_length + tap_hint.word_space,
-            //         tap_hint.final_pos.y,
-            //     );
-            //     p5.pop();
-            //     //
-            //     // // for DEBUG:
-            //     // p5.push();
-            //     // p5.fill(243, 0, 1);
-            //     // p5.noStroke();
-            //     // p5.rect(
-            //     //     tap_hint.final_pos.x, //
-            //     //     tap_hint.final_pos.y,
-            //     //     tap_hint.total_width,
-            //     //     1,
-            //     // );
-            //     // p5.pop();
-            //     //
-            //     p5.pop();
-            //     // }
-            // }
-            // //
+            //
+            //  MARK: "tap" hint animation (TODO)
+            //
+            if (_now > 1000 * 5.0) {
+                var progress_sec = (_now - 1000 * 5.0) * 0.001;
+                progress_sec = progress_sec * 2.0;
+                // console.log("progress_sec:", progress_sec);
+                tap_hint.opacity = Math.min(1.0, progress_sec / 1.0);
+                // tap_hint.opacity = Math.pow(tap_hint.opacity, 5.0);
+                console.log("tap_hint.opacity:", tap_hint.opacity);
+                // tap_hint.opacity = Math.pow(Math.min(1.0, Math.max(0.0, (_now - 1000 * tap_hint.hint_delay_sec) * 0.00004)), 3.0);
+                // console.log("tap_hint.opacity:", tap_hint.opacity);
+                // tap_hint.opacity = Max.max(1.0, )
+                var tap_hint_color = p5.color(243, 242, 243);
+                tap_hint_color.setAlpha(tap_hint.opacity * 255);
+                //
+                p5.push();
+                p5.noFill();
+                p5.stroke(tap_hint_color);
+                p5.strokeWeight(2);
+                //
+                p5.push();
+                var tap_circ_progress = Math.sin(_now * 0.004 * tap_hint.speed) * 0.5 + 0.5;
+                tap_circ_progress = 1.0 - Math.pow(1.0 - tap_circ_progress, 3.0);
+                tap_hint.radius_current = tap_hint.radius + tap_hint.radius_variance * (tap_circ_progress - 0.5);
+                p5.circle(tap_hint.final_pos.x, tap_hint.final_pos.y, tap_hint.radius_current * 2.0);
+                p5.pop();
+                //
+                p5.push();
+                p5.line(
+                    tap_hint.final_pos.x + tap_hint.radius_current + tap_hint.line_space, //
+                    // tap_hint.final_pos.x + tap_hint.radius + tap_hint.line_space, //
+                    tap_hint.final_pos.y,
+                    tap_hint.final_pos.x + tap_hint.radius + tap_hint.line_length,
+                    tap_hint.final_pos.y,
+                );
+                p5.pop();
+                //
+                p5.push();
+                p5.fill(tap_hint_color);
+                p5.noStroke();
+                p5.text(
+                    tap_hint.text, //
+                    tap_hint.final_pos.x + tap_hint.radius + tap_hint.line_length + tap_hint.word_space,
+                    tap_hint.final_pos.y,
+                );
+                p5.pop();
+                //
+                // // for DEBUG:
+                // p5.push();
+                // p5.fill(243, 0, 1);
+                // p5.noStroke();
+                // p5.rect(
+                //     tap_hint.final_pos.x, //
+                //     tap_hint.final_pos.y,
+                //     tap_hint.total_width,
+                //     1,
+                // );
+                // p5.pop();
+                //
+                p5.pop();
+                // }
+            }
+            //
 
             //
             p5.pop();
